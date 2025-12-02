8059cbe14b432f690177cb604a6657b02255f5d4
I.R. COMERCIO E MATERIAIS ELETRICOS LTDA <miguel.alves1@faculdadegran.edu.br>
2025-12-02 15:00:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Configuração do Supabase
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY
+    process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 // Middlewares
